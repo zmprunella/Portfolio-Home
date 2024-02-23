ea1ce9b7d7e70d10b7d37a6768974908fb7c239e
Zane Prunella <102100627+zmprunella@users.noreply.github.com>
2024-02-23 15:51:06 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,22 +6,7 @@ const About = () => {
     <section className="about">
       <h2>About Me</h2>
       <p>
-        Hello! I'm an experienced software developer 💻 with a passion for
-        designing innovative applications. I'm deeply curious about how
-        technology connects our world, and I'm always seeking new ways to
-        contribute my skills to interesting projects ⚙️ . My Background: I bring
-        over seven years of professional experience across diverse industries,
-        including audio/music production 🎧 , sales 🚗 , and aviation 🛩. This
-        unique background shapes my approach to software development, providing
-        me with a broad perspective and a deep understanding of different user
-        needs. My Skills: I'm adept at working in fast-paced environments,
-        adapting quickly to new situations, and solving complex problems with
-        analytical and strategic thinking. My technical skills include
-        JavaScript, React, Vue, WordPress, Hubspot, Github, and Git 💿. My Goal:
-        I'm eager to find opportunities where I can contribute creatively and
-        collaboratively to projects that value cutting-edge technology. If
-        you're looking for a developer who can bring a fresh perspective and a
-        strong technical skillset, let's connect! ☀️
+With a strong foundation in JavaScript, React, Vue, and Svelte, I am a skilled front-end developer with over 2 years of technical experience and over 5 years of industry experience. My expertise lies in creating dynamic web interfaces and rich user experiences, leveraging these advanced front-end technologies to bring innovative web solutions to life. 
       </p>
     </section>
   );
